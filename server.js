a0e91b4e6fd1304431a32b4ea036e6ebbeb89972
juliuskdjnr <juliuskdjnr@gmail.com>
2025-07-28 12:53:05 +0000

    Update server.js
    A little route for a trip I took on the 26th and 27th of July 2025.

--- server.js
+++ server.js
@@ -47,6 +47,16 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product);
 });
 
+app.get('/trips', (req, res) => {
+    const trips = [
+        { travel_route: 'Wa - Nandom - Burkina Faso' },
+        { duration: '2 days' },
+        { summary: 'Set out for a post burial celebration of my grandparents. \nHeld in Wesa, Burkina Faso, we set out from home in Wa and made our way to Nandom. On arrival, our car broke down. Got fixed and an hour later, left to Wesa. \nArrived finally in Wesa after an hours drive and attended the mass. \nAfter, we had a little refreshment with family from far and near. At 5:30 PM, we set off back to Nandom. \n Slept over there and attended church service the next day. \nLater that day at 3:00 PM, we got back on the road to Wa. \nAfter a long journey on the rough road, we made it back home. \nAs the chief driver of this trip, I definitely got to experience travelling a whole lot differently and this makes this trip worth remebering.' },
+    ];
+
+    res.json(trips);
+});
+
 // Start server
 app.listen(3000, () => {
   console.log('Express server running at http://localhost:3000');
